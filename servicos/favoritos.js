0bf39ef9678d960a88463c6b07b93c444afdc2a9
Jheimys Bassani <jheimysbb@gmail.com>
2023-10-06 18:26:05 +0100

    :bug: Fixing bugs

--- servicos/favoritos.js
+++ servicos/favoritos.js
@@ -1,26 +1,26 @@
 const fs = require('fs')
 
 function getTodosFavoritos() {
-    return JSON.parse(fs.readFileSync('favoritos.js'))
+    return JSON.parse(fs.readFileSync('favoritos.json'))
 }
 
 function deletaFavoritosPorId(id) {
-    const livros = JSON.parse(fs.readFileSync('favoritos.js'))
+    const livros = JSON.parse(fs.readFileSync('favoritos.json'))
 
     const livrosFiltrados = livros.filter(livro => livro.id !== id)
 
-    fs.writeFileSync('favoritos.js', JSON.stringify(livrosFiltrados))
+    fs.writeFileSync('favoritos.json', JSON.stringify(livrosFiltrados))
 }
 
 function insereLivro(id) {
-    const livros = JSON.parse(fs.readFileSync('livros.js'))
-    const favoritos = JSON.parse(fs.readFileSync('favoritos.js'))
+    const livros = JSON.parse(fs.readFileSync('livros.json'))
+    const favoritos = JSON.parse(fs.readFileSync('favoritos.json'))
 
     const livrosInseridos = livros.find(livro => livro.id === id)
 
     const novaListaDeLivrosFavoritos = [...favoritos, livrosInseridos]
 
-    fs.writeFileSync('favoritos.json', JSON.stringify('novaListaDeLivrosFavoritos'))
+    fs.writeFileSync('favoritos.json', JSON.stringify(novaListaDeLivrosFavoritos))
 }
 
 module.exports ={
